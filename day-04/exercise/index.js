77526207ce0ca41a46bf5ab13fdc35927edd6b16
Nguyen Xuan Phong <ngxphong03@gmail.com>
2024-06-02 09:03:24 +0700

    First commit exercise day04

--- day-04/exercise/index.js
+++ day-04/exercise/index.js
@@ -1,27 +1,30 @@
 var content =
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit cumque eaque itaque debitis quae repellat iusto exercitationem ab ullam Similique vel aliquam aliquid quidem dolores neque nulla";
-var arrContent = content.split(" ");
-console.log(arrContent);
-let position;
-let keyword;
-let indexCurrentWord = 0;
-let indexCurrentHighlight = 0;
-setInterval(function () {
-  keyword = arrContent[indexCurrentWord];
-  let regex = new RegExp(`\\b${keyword}\\b`); // Tạo biểu thức chính quy để tìm từ khóa là một từ riêng biệt
-  let match = content.match(regex); // Tìm kiếm từ khóa trong nội dung
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit cumque eaque itaque et debitis at a quae repellat iusto exercitationem ab ullam Similique vel aliquam aliquid quidem dolores neque nulla";
+var keyword = " ";
+var startIndex = 0;
+var endIndex = content.indexOf(keyword);
+var isCurrentLastIndex = false;
 
-  if (match) {
-    position = match.index;
-    let newContent =
-      content.slice(0, position) +
-      `<span>${content.slice(position, position + keyword.length)}</span>` +
-      content.slice(position + keyword.length);
-    document.body.innerHTML = newContent;
-  }
-  if (indexCurrentWord < arrContent.length - 1) {
-    indexCurrentWord++;
+setInterval(function () {
+  var newContent;
+  if (isCurrentLastIndex) {
+    newContent =
+      content.slice(0, content.lastIndexOf(keyword)) +
+      `<span>${content.slice(content.lastIndexOf(keyword))}</span>`;
+    startIndex = 0;
+    endIndex = content.indexOf(keyword);
+    isCurrentLastIndex = false;
   } else {
-    indexCurrentWord = 0;
+    isCurrentLastIndex = content.lastIndexOf(keyword) === endIndex;
+    newContent =
+      content.slice(0, startIndex) +
+      `<span>${content.slice(startIndex, endIndex)}</span>` +
+      content.slice(endIndex);
+    startIndex = endIndex;
+    endIndex =
+      content.slice(startIndex + 1).indexOf(keyword) + (startIndex + 1);
+    console.log(isCurrentLastIndex);
   }
+
+  document.body.innerHTML = newContent;
 }, 500);
