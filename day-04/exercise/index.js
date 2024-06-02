611f4a7542936a44218e5e37a603c0bea40c6c2e
Nguyen Xuan Phong <ngxphong03@gmail.com>
2024-06-02 21:36:16 +0700

    Fix bug ex04 third commit

--- day-04/exercise/index.js
+++ day-04/exercise/index.js
@@ -7,10 +7,17 @@ var isCurrentLastIndex = false;
 
 setInterval(function () {
   var newContent;
+
+  while (endIndex !== -1 && content.slice(startIndex, endIndex).trim() === "") {
+    startIndex = endIndex + 1;
+    endIndex = content.indexOf(keyword, startIndex);
+  }
+
   if (isCurrentLastIndex) {
     newContent =
       content.slice(0, content.lastIndexOf(keyword)) +
       `<span>${content.slice(content.lastIndexOf(keyword))}</span>`;
+
     startIndex = 0;
     endIndex = content.indexOf(keyword);
     isCurrentLastIndex = false;
@@ -20,10 +27,18 @@ setInterval(function () {
       content.slice(0, startIndex) +
       `<span>${content.slice(startIndex, endIndex)}</span>` +
       content.slice(endIndex);
+
     startIndex = endIndex;
     endIndex =
       content.slice(startIndex + 1).indexOf(keyword) + (startIndex + 1);
-    console.log(isCurrentLastIndex);
+
+    while (
+      endIndex !== -1 &&
+      content.slice(startIndex, endIndex).trim() === ""
+    ) {
+      startIndex = endIndex + 1;
+      endIndex = content.indexOf(keyword, startIndex);
+    }
   }
 
   document.body.innerHTML = newContent;
