9b83147e483e954dc259f6d6ead2310c13d74f77
Michael Nguyen <ngxphong03@gmail.com>
2024-07-11 15:11:30 +0700

    update

--- dom-node/exercise/day-01/constants.js
+++
@@ -1,6 +0,0 @@
-const ADD = "add_product";
-const REDUCE = "reduce_product";
-const REMOVE = "remove_product";
-const CART_DATA = JSON.parse(localStorage.getItem("cart")) || [];
-
-export { ADD, REDUCE, REMOVE, CART_DATA };
