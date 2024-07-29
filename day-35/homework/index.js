0d24dd49545552bff09d22e48af38f176621bb76
Michael Nguyen <ngxphong03@gmail.com>
2024-07-29 19:21:59 +0700

    feat: ngưng thời gian khi chuyển tab

--- day-35/homework/index.js
+++ day-35/homework/index.js
@@ -1,27 +1,41 @@
-var getLinkBtnEl = document.querySelector(".getLinkBtn");
-var isCompletedCountDown = false;
+const timerEl = document.querySelector("#timer");
+const getLinkBtn = document.querySelector(".getLinkBtn");
 
-function countdown(countDownTime, countDownBox) {
-  var startTime = Date.now();
-  var endTime = startTime + countDownTime * 1000;
+let countDownTime = 30;
+let startTime = Date.now();
+let endTime = startTime + countDownTime * 1000;
+let stopTime;
+let requestAnimationFrameId;
 
-  function update() {
-    var currentTime = Date.now();
-    var remaining = Math.max(0, endTime - currentTime);
-    var seconds = Math.floor(remaining / 1000);
-    countDownBox.textContent = seconds;
-    if (remaining > 0) {
-      requestAnimationFrame(update);
-    } else {
-      getLinkBtnEl.style.cursor = "pointer";
-      getLinkBtnEl.style.background = "#03bdf0";
-      getLinkBtnEl.addEventListener("click", function () {
-        window.location.href = "https://fullstack.edu.vn/";
-      });
-    }
+function startCountDown() {
+  let currentTime = Date.now();
+  let remainingTime = Math.max(0, endTime - currentTime);
+  timerEl.innerText = Math.floor(remainingTime / 1000);
+  if (remainingTime > 0) {
+    requestAnimationFrameId = requestAnimationFrame(startCountDown);
+  } else {
+    const css = {
+      cursor: "pointer",
+      background: "03BDF0",
+    };
+    Object.assign(timerEl.style, css);
   }
-  update();
 }
 
-var displayElement = document.getElementById("timer");
-countdown(30, displayElement);
+function stopCountDown() {
+  // Đánh dấu mốc tgian chuyển tab và ngừng đếm ngược
+  stopTime = Date.now();
+  cancelAnimationFrame(requestAnimationFrameId);
+}
+
+document.addEventListener("visibilitychange", function () {
+  if (document.visibilityState === "hidden") {
+    stopCountDown();
+  } else {
+    // Nếu có Stop time thì mới cập nhật lại end time
+    endTime += stopTime ? Date.now() - stopTime : 0;
+    startCountDown();
+  }
+});
+
+startCountDown();
