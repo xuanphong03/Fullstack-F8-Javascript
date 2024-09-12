6d43ddd6f5e1dbebd5377eb24fbd11dde3aa78d4
Michael Nguyen <ngxphong03@gmail.com>
2024-09-12 20:17:27 +0700

    update Error page

--- day-46/exercise-module-bundler/src/Error.js
+++ day-46/exercise-module-bundler/src/Error.js
@@ -1,5 +1,8 @@
 function Error() {
-  return `<h1>Error 404 Not found</h1>`;
+  return `
+  <div class='w-full h-screen'>
+    <img src='https://colorlib.com/wp/wp-content/uploads/sites/2/404-error-page-templates.jpg' class='w-full max-h-full'  />
+  </div>`;
 }
 
 export default Error;
