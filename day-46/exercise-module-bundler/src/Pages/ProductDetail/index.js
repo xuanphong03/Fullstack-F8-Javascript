35f08cecc99c6d53cc4338e300ff49901f882ed2
Michael Nguyen <ngxphong03@gmail.com>
2024-09-12 20:13:03 +0700

    config error 404

--- day-46/exercise-module-bundler/src/Pages/ProductDetail/index.js
+++ day-46/exercise-module-bundler/src/Pages/ProductDetail/index.js
@@ -1,6 +1,10 @@
-function ProductDetail({ id }) {
+function ProductDetail(url) {
+  const {
+    data: { id },
+  } = url;
+
   return `
-    <h1 class='text-2xl font-medium text-blue-500'>Chi tiết sản phẩm: ${id}</h1>
+    <h1 class='text-2xl font-medium text-blue-500'>Chi tiết sản phẩm: ${id} </h1>
     <button><a class='px-5 py-1 text-white bg-blue-500 block rounded mt-2' href='/san-pham' data-route>Back</a></button>
   `;
 }
--- day-46/exercise-module-bundler/src/Utils/router.js
+++ day-46/exercise-module-bundler/src/Utils/router.js
@@ -4,17 +4,19 @@ import Error from "../Error";
 const app = document.querySelector("#app");
 const routerNavigo = new Navigo("/", { linksSelector: "a", hash: false });
 
-const renderApp = (page, defaultLayout) => {
-  app.innerHTML = defaultLayout.replace("{body}", page);
+const renderApp = (page, defaultLayout, isNotFound = false) => {
+  if (!isNotFound) {
+    return (app.innerHTML = defaultLayout.replace("{body}", page));
+  }
+  return (app.innerHTML = page);
 };
 
 export const router = (routes = [], defaultLayout) => {
   routes.forEach(({ path, component }) => {
-    routerNavigo.on(path, (urlData) => {
-      const { data: params } = urlData;
-      renderApp(component(params), defaultLayout(), urlData);
+    routerNavigo.on(path, (url) => {
+      renderApp(component(url), defaultLayout());
     });
   });
-  routerNavigo.notFound(() => renderApp(Error(), defaultLayout()));
+  routerNavigo.notFound(() => renderApp(Error(), defaultLayout(), true));
   routerNavigo.resolve();
 };
